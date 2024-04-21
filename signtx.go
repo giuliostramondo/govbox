4f0633daf0bd0338d1dac9ac2e2d47a77b3eb9ed
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2024-04-21 20:30:36 +0200

    find unregistered @types
    find these types is straighforward, register them at runtime will
    probably be challenging...

--- signtx.go
+++ signtx.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"slices"
+
+	"github.com/davecgh/go-spew/spew"
 
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 )
@@ -14,11 +17,49 @@ func signTx(unsignedTxFile string) error {
 		return err
 	}
 	defer f.Close()
-	var tx txtypes.Tx
-	// NOTE: Unsurprisingly doesn't work with legacy encoding/json
-	if err := json.NewDecoder(f).Decode(&tx); err != nil {
-		return err
+	var rawTx struct {
+		Body struct {
+			Messages []map[string]any
+		}
 	}
-	fmt.Println(tx)
+	if err := json.NewDecoder(f).Decode(&rawTx); err != nil {
+		return fmt.Errorf("JSON decode %s: %v", unsignedTxFile, err)
+	}
+	var types []string
+	for _, msg := range rawTx.Body.Messages {
+		findUnregisteredTypes(msg, &types)
+	}
+	fmt.Println("TODO: NEED TO REGISTER THESE TYPES", types)
+	var tx txtypes.Tx
+	if _, err := f.Seek(0, 0); err != nil {
+		return fmt.Errorf("seek %s: %v", unsignedTxFile, err)
+	}
+	if err := unmarshaler.Unmarshal(f, &tx); err != nil {
+		return fmt.Errorf("unmarshal tx: %v", err)
+	}
+	spew.Dump(tx)
 	return nil
+}
+
+func findUnregisteredTypes(m map[string]any, types *[]string) {
+	for k, v := range m {
+		if k == "@type" {
+			typeURL := v.(string)
+			if _, err := registry.Resolve(typeURL); err != nil {
+				if !slices.Contains(*types, typeURL) {
+					// type not registered, add to the list
+					*types = append(*types, typeURL)
+				}
+			}
+			continue
+		}
+		switch x := v.(type) {
+		case []map[string]any:
+			for _, m := range x {
+				findUnregisteredTypes(m, types)
+			}
+		case map[string]any:
+			findUnregisteredTypes(x, types)
+		}
+	}
 }
